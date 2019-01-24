ae7f957747346b546d01c7ccb15efdf41d0cdf2d
Matej69 <mlelek94@gmail.com>
2019-01-25 00:25:04 +0100

    All derived weapons classes deleted, all weapons are now created by creating Weapon class on GameObject and setting proper inspector values

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 
 public class Weapon : MonoBehaviour {
 
+  [Serializable]
+  public struct MinMaxPair
+  {
+    public float min, max;
+  }
+  
     public enum E_WEAPON_TYPE { GUN, SHOTGUN, MINIGUN, GRANADE, UZI, SPACEGUN, BFG, GRANADE_LUNCHER }
     public enum E_FIRE_TYPE { ON_HOLD, ON_TOUCH }
     public E_WEAPON_TYPE weaponType;
@@ -12,6 +19,15 @@ public class Weapon : MonoBehaviour {
     protected GameObject ownerSpriteObj;
     public GameObject pref_bullet;
     public float reloadTime;
+    [Space(10)]
+    public int numOfBullets;
+    public MinMaxPair xRandVelocity;
+    public MinMaxPair yRandVelocity;
+    [Space(10)]
+    public float shakeMagnitude;
+    public float shakeTime;
+    public float shakeEveryXSec;
+
 
     protected GameObject bulletsSpawnObj;
     protected Timer timer_automaticShooting;
@@ -53,9 +69,13 @@ public class Weapon : MonoBehaviour {
     
     public virtual void SpawnBullet()
     {
+      CameraController.Shake(shakeMagnitude, shakeTime, shakeEveryXSec);
+      for(int i = 0; i < numOfBullets; ++i) { 
         GameObject bullet = (GameObject)Instantiate(pref_bullet, bulletsSpawnObj.transform.position, Quaternion.identity);
+        bullet.GetComponent<Bullet>().velocity = new Vector2(UnityEngine.Random.Range(xRandVelocity.min, xRandVelocity.max), UnityEngine.Random.Range(yRandVelocity.min, yRandVelocity.max));
         bullet.GetComponent<Bullet>().SetBulletDirection((int)ownerSpriteObj.transform.localScale.x);
-    }
+      }
+  }
 
 
 
